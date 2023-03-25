877622fe30e237bed9bd00957d0e642f0089548a
Giles Booth <giles.booth@gmail.com>
2023-03-25 09:36:24 +0000

    Update ssd1306_bitmap.py

--- ssd1306_bitmap.py
+++ ssd1306_bitmap.py
@@ -9,5 +9,5 @@ def show_bitmap(filename):
     with open(filename, 'rb') as my_file:
         for i in range(0, 16):
             i2c.write(ADDR, b'\x40' + my_file.read(64))
-    set_zoom(0)
+#    set_zoom(0)
     command([0xaf])
