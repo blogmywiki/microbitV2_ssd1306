660c3049104165128e229e0d993f5df0ddf73136
Giles Booth <giles.booth@gmail.com>
2023-03-26 14:16:39 +0100

    Update ssd1306_bitmap.py

--- ssd1306_bitmap.py
+++ ssd1306_bitmap.py
@@ -1,13 +1,14 @@
-from microbit import i2c
+# bitmaps from original library may be shown inverted
 
+from microbit import i2c
 from ssd1306 import command, set_zoom, set_pos, ADDR
 
 
 def show_bitmap(filename):
     set_pos()
-    command([0xae])
+    command([0xae]) # screen off
     with open(filename, 'rb') as my_file:
         for i in range(0, 16):
             i2c.write(ADDR, b'\x40' + my_file.read(64))
 #    set_zoom(0)
-    command([0xaf])
+    command([0xaf]) # screen on
